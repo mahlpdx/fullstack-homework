01e04111ddc1c34f4a2835acdab5e6a25f77c500
TimmyYoung <77657486+TimmyYoung@users.noreply.github.com>
2022-10-31 22:21:07 -0700

    Finished exercise #2.

--- hw3/02-url.js
+++ hw3/02-url.js
@@ -14,28 +14,36 @@ const server = http.createServer((req, res) => {
 
   let getRoutes = () => {
     let result = '';
-
     routes.forEach(
       (elem) => (result += `<li><a href="${elem}">${elem}</a></li>`)
     );
-
     return result;
   };
-
   if (req.url === '/') {
     let routeResults = getRoutes();
-
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write(`<h1>Exercise 02</h1>`);
-
     res.write(`<ul> ${routeResults} </ul>`);
   }
-
-  // Add your code here
-
+  // Add your code here////////////////////////////////////////////////////////////////
+  else {
+  res.write(`<table border="2">`); //Calling for a html table with a fixed border size.
+  res.write(`<tr style=vertical-align:top>`); //Tag to allign table via css.
+  res.write('<th> Parameter 1 </th>'); //Header for first column of table.
+  res.write('<th> Parameter 2 </th>'); //Header for second column of table.
+  res.write('</tr>');}// End of table formatting for header. <tr> is a simple table.
+  url.searchParams.forEach((value, key) => {
+    res.write('<tr>'); //Begin table formatting for table data.
+    res.write(`<td> ${key} </td>`); //Table data 1, aka data row #1.
+    res.write(`<td> ${value} </td>`); //Table data 2, aka data row #2.
+    res.write('</tr>'); //End table data for <tr> type table.
+  });
+  res.write('</table>'); // End table.
   res.end();
 });
+////////////////////////////////////////////////////////////////////////////////////////
 
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
