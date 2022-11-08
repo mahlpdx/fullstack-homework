7467782c8a6fcce4a34167f182e3cacc95f103f0
Drew Mahler <drewmahler@Drews-MacBook-Pro-2.local>
2022-11-08 14:35:24 -0800

    Completed exercise hw4/03-form.js and formatted with prettier.

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,5 +1,7 @@
+const e = require('express');
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 5001;
 
 // Use middleware static() to serve all static files in the given folder
@@ -8,9 +10,23 @@ app.use(express.static('public'));
 // Use middleware urlencoded() to parse an incoming request with a urlencoded payload and return an objectß
 app.use(express.urlencoded({ extended: false }));
 
-// POST request
-app.post('', (req, res) => {
-  // Add your code here
+// GET request
+app.get('/form', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.post('/submit', (req, res) => {
+  res.set("Content-Type", "text/html")
+  res.write(`Name: ${req.body.name}<br>`);
+  res.write(`Email: ${req.body.email}<br>`);
+  res.write(`Comment: ${req.body.message}<br>`);
+  if (req.body.checkbox) {
+    res.write(`Checkbox: Yes, sign me up for the newsletter.<br>`);
+  }
+  else {
+    res.write(`Checkbox: No, thank you.<br>`); 
+  }
+  res.end()
 });
 
 app.listen(port, () => {
