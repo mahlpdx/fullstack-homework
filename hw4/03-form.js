c69896810385b2643834c44780a75b1726fda2f7
Drew Mahler <drewmahler@Drews-MacBook-Pro-2.local>
2022-11-09 12:25:21 -0800

    Formatted 03-form.js and 04-template.js. Completed functionality for 04-template.js

--- hw4/03-form.js
+++ hw4/03-form.js
@@ -1,32 +1,31 @@
-const e = require('express');
-const express = require('express');
+const e = require("express");
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = process.env.PORT || 5001;
 
 // Use middleware static() to serve all static files in the given folder
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Use middleware urlencoded() to parse an incoming request with a urlencoded payload and return an objectß
 app.use(express.urlencoded({ extended: false }));
 
 // GET request
-app.get('/form', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+app.get("/form", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
-app.post('/submit', (req, res) => {
-  res.set("Content-Type", "text/html")
+app.post("/submit", (req, res) => {
+  res.set("Content-Type", "text/html");
   res.write(`Name: ${req.body.name}<br>`);
   res.write(`Email: ${req.body.email}<br>`);
   res.write(`Comment: ${req.body.message}<br>`);
   if (req.body.checkbox) {
     res.write(`Checkbox: Yes, sign me up for the newsletter.<br>`);
+  } else {
+    res.write(`Checkbox: No, thank you.<br>`);
   }
-  else {
-    res.write(`Checkbox: No, thank you.<br>`); 
-  }
-  res.end()
+  res.end();
 });
 
 app.listen(port, () => {
--- hw4/04-templates.js
+++ hw4/04-templates.js
@@ -1,62 +1,135 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
+const e = require("express");
 
 const app = express();
 const port = process.env.PORT || 5001;
 
 // Use Pug as the templating engine
-app.set('views', __dirname + '/views');
-app.set('view engine', 'pug');
+app.set("views", __dirname + "/views");
+app.set("view engine", "pug");
 
 // REST Countries URL
-const url = 'https://restcountries.com/v3.1/all';
+const url = "https://restcountries.com/v3.1/all";
 
 // Add your code here
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   // render pug template for the index.html file
 
-  res.render('index', {
-    heading: 'Countries of the World',
-    main: 'Welcome to this application. Using the REST Countries API, we will be showing the countries and capitals of the world, the most populous countries in the world, and the number of countries in each region of the world',
+  res.render("index", {
+    heading: "Countries of the World",
+    main: "Welcome to this application. Using the REST Countries API, we will be showing the countries and capitals of the world, the most populous countries in the world, and the number of countries in each region of the world",
   });
 });
 
-app.get('/capitals', (req, res) => {
+app.get("/capitals", (req, res) => {
   // map the output array to create an array with country names and capitals
   // check for empty data in the output array
 
-  let countries = ['Afghanistan', 'Aland Islands', 'Albania'];
-
-  res.render('page', {
-    heading: 'Countries and Capitals',
-    results: countries,
-  });
+  axios
+    .get(url)
+    .then((response) => {
+      let countries = [];
+      // Loop through response and create formatted string
+      response.data.forEach((el) => {
+        countries.push(`${el.name.common} - ${el.capital}`);
+      });
+      // Sort alphabetically
+      countries.sort();
+      // Render page
+      res.render("page", {
+        heading: "Countries and Capitals",
+        results: countries,
+      });
+    })
+    .catch((err) =>
+      // Render error case
+      res.render(
+        res.render("page", {
+          heading: "Countries and Capitals",
+          results: ["Unable to parse API"],
+        })
+      )
+    );
 });
 
-app.get('/populous', (req, res) => {
+app.get("/populous", (req, res) => {
   // filter the output array for the countries with population of 50 million or more
   // sort the resulting array to show the results in order of population
   // map the resulting array into a new array with the country name and formatted population
 
-  let populous = ['China', 'India', 'United States of America'];
-
-  res.render('page', {
-    heading: 'Most Populous Countries',
-    results: populous,
-  });
+  axios
+    .get(url)
+    .then((response) => {
+      // Filter results
+      let output = response.data.filter((el) => el.population > 50000000);
+      // Sort results
+      output.sort((a, b) => {
+        a.population > b.population;
+      });
+      // Format results
+      let formatted = [];
+      output.forEach((el) => {
+        formatted.push(
+          `${el.name.common} - ${el.population.toLocaleString("en-US")}`
+        );
+      });
+      // Render page
+      res.render("page", {
+        heading: "Most Populous Countries",
+        results: formatted,
+      });
+    })
+    .catch((err) =>
+      // Render error case
+      res.render(
+        res.render("page", {
+          heading: "Most Populous Countries",
+          results: ["Unable to parse API"],
+        })
+      )
+    );
 });
 
-app.get('/regions', (req, res) => {
+app.get("/regions", (req, res) => {
   // reduce the output array in a resulting object that will feature the numbers of countries in each region
   // disregard empty data from the output array
 
-  let regions = ['Asia - 50', 'Europe - 53', 'Africa - 60'];
-
-  res.render('page', {
-    heading: 'Regions of the World',
-    results: regions,
-  });
+  // API Call
+  axios
+    .get(url)
+    .then((response) => {
+      let output = response.data;
+      let regions = {};
+      // Create counts for each region
+      output.forEach((el) => {
+        if (el.region in regions) {
+          regions[el.region] += 1;
+        } else {
+          regions[el.region] = 1;
+        }
+      });
+      // Create formatted string for webpage
+      let formatted = [];
+      Object.entries(regions).forEach(([key, value]) =>
+        formatted.push(`${key} - ${value.toLocaleString("en-US")}`)
+      );
+      // Render page
+      res.render("page", {
+        heading: "Regions of the World",
+        results: formatted,
+      });
+    })
+    .catch((err) =>
+      // Render error case
+      res.render(
+        res.render("page", {
+          heading: "Regions of the World",
+          results: ["Unable to parse API"],
+        })
+      )
+    );
 });
 
 app.listen(port, () => {
